a9ad4f42eb3c634e164686b63153a95d8452f3f4
maggie <maggie.weng112@gmail.com>
2024-04-06 23:41:58 -0700

    placeholder model

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,19 +1,19 @@
-//import mongoose, { Document, Schema } from 'mongoose';
+import mongoose, { Document, Schema } from "mongoose";
 
-//interface IUser extends Document {
-//  username: string;
-//  email: string;
-//  password: string;
-//}
+interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+}
 
-//const UserSchema = new Schema({
-//  username: { type: String, required: true },
-//  email: { type: String, required: true, unique: true },
-//  password: { type: String, required: true },
-//});
+const UserSchema = new Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
 
-//const User = mongoose.model<IUser>('User', UserSchema);
+const User = mongoose.model<IUser>("User", UserSchema);
 
-//export default User;
+export default User;
 
 // example model
